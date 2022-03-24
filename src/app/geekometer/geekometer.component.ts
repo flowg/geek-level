075e29e307b74b1931519143975353a59d75c057
Florian Gesta <flo.gesta@gmail.com>
2022-03-24 16:32:44 +0100

    The pointer moves accordingly to provided percentages variations and stays in range

--- src/app/geekometer/geekometer.component.ts
+++ src/app/geekometer/geekometer.component.ts
@@ -3,27 +3,35 @@
  */
 import { Component, OnInit } from '@angular/core';
 
+/**
+ * TypeScript entities and constants
+ */
+const angleOffset = 20;
+
 @Component( {
   selector: 'app-geekometer',
   templateUrl: './geekometer.component.html',
   styleUrls: ['./geekometer.component.scss']
 } )
 export class GeekometerComponent implements OnInit {
-  angle: number | undefined;
-
-  private angleOffset = 20;
+  angle = angleOffset;
 
   ngOnInit(): void {
     this.computePointerAngle();
   }
 
-  private computePointerAngle(variation?: number): void {
-    if ( !variation ) {
-      this.angle = this.angle ?? this.angleOffset;
-
-      return;
+  computePointerAngle( percentageVariation?: number ): void {
+    if ( percentageVariation ) {
+      const amplitude = 360 - 2 * angleOffset;
+      const degreeVariation = ( amplitude * percentageVariation ) / 200;
+      const expectedAngle = this.angle + degreeVariation;
+      if ( expectedAngle < angleOffset ) {
+        this.angle = angleOffset;
+      } else if ( expectedAngle > 360 - angleOffset ) {
+        this.angle = 360 - angleOffset;
+      } else {
+        this.angle = expectedAngle;
+      }
     }
-
-    const amplitude = 360 - 2 * this.angleOffset;
   }
 }
